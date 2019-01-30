bc60e40eddb1da804a4b6e2cc980f1c9a7c8902d
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-30 22:17:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  var newObj = object.assign({}, object)
-  delete newObj.key
-   return newObj
+  var newObj = Object.assign({}, object)
+  delete newObj.key 
+  return newObj
 }
